8abe0fa308f4e608090d46c3d8b9ddb7fd5fbdc4
ivansimo <ivansimo@industria40.me>
2019-03-04 10:43:40 +0100

    Added local settings import

--- remote/settings.py
+++ remote/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='isimosanchez@gmail.com'
 EMAIL_HOST_PASSWORD='hacedece79'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
