d72d3f465dffc314871b04a9b02835e598faa903
ivansimo <ivansimo@industria40.me>
2019-03-04 12:16:49 +0100

    prueba push

--- grupos/admin.py
+++ grupos/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Grupo
 
 class GrupoAdmin(admin.ModelAdmin):
-    list_display = ('cliente','modelo','ciudad','puesta_en_marcha','averiado')
-    list_display_links = ('cliente','puesta_en_marcha')
+    list_display = ('modelo','cliente','ciudad','puesta_en_marcha','averiado')
+    list_display_links = ('modelo','puesta_en_marcha')
     list_filter = ('ciudad',)
     list_editable = ('averiado',)
     search_fields = ('marca','cp')
